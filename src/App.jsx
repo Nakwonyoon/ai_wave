b347054564824be03de3cef21b8286cbc44fae99
nakwonyoon <yoonnw1000@gmail.com>
2024-03-04 14:54:03 +0900

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import Hero from './components/Hero';
 const App = () => {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>
-        Hello, World
-      </h1>
+     
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
         <Header/>
         <Hero/>
